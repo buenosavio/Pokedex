580261c7a116321d29bfa25ec00057f657699673
César Fernandes Luchesi <cesarfluchesi123@gmail.com>
2022-04-18 20:20:02 -0300

    DTO

--- pokedex/src/store/reducers/PokedexReducer.tsx
+++ pokedex/src/store/reducers/PokedexReducer.tsx
@@ -1,14 +1,43 @@
-const INITIAL_VALUES = {
-  activePokemon: {},
-  data: [],
-  details: []
+import { InitialDTO } from "../../models/PokemonDTO"
+
+const INITIAL_VALUES:InitialDTO = {
+  activePokemon:{},
+        data: [
+          {
+            id: 0,
+            name: "",
+            typename: [
+              {
+              type:{
+                name: "",
+              }
+              }
+            ],
+            weight: 0,
+            height: 0,
+            habilities: [
+              {
+                name: "",
+                base_stat: 0,
+              }
+            ],
+            moviments:   {        
+                name: "",   
+            } ,
+            
+            flavor: "",
+          }],
+        loading: true,
+        error: false,
 }
 
-const PokedexReducer = (state = INITIAL_VALUES, action: any) => {
+const PokedexReducer = (state:InitialDTO = INITIAL_VALUES, action: any) => {
   if (action.type === 'GET_POKEDEX') {
     return {
       ...state,
-      data: action.newPokeList
+      data: action.newPokeList,
+      loading: action.loading,
+      error: action.error,
     }
   }
   if(action.type === 'GET_DETAILS'){
